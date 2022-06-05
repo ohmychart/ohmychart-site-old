c9046f2f9ae48b7f568dc033804bd11e676d38c0
d2ski <chukotski@gmail.com>
2022-06-05 15:40:01 +0200

    Remove ignored files

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/pages/__layout.svelte.js';
-
-export { module };
-export const entry = 'pages/__layout.svelte-696c64a9.js';
-export const js = ["pages/__layout.svelte-696c64a9.js","chunks/vendor-306961a8.js","chunks/paths-4b3c6e7e.js"];
-export const css = ["assets/pages/__layout.svelte-9ae70fcf.css"];
--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/pages/error.svelte.js';
-
-export { module };
-export const entry = 'error.svelte-7325e280.js';
-export const js = ["error.svelte-7325e280.js","chunks/vendor-306961a8.js"];
-export const css = [];
